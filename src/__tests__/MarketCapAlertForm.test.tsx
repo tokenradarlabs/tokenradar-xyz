68ca1e5c48cfd2de6ab74b97ba9123320716a902
Simon Smilga <simon0x1800@gmail.com>
2025-11-13 13:51:35 +0000

    Fix: Theme toggle hydration and add system theme cycle

--- src/__tests__/MarketCapAlertForm.test.tsx
+++ src/__tests__/MarketCapAlertForm.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import MarketCapAlertForm from "../components/MarketCapAlertForm/MarketCapAlertForm";
 import "@testing-library/jest-dom";
